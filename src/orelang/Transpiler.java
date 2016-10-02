97fbc39ecce399556a49908eb7960a2eb104337e
Shuetsu Ito <shuetsu512@gmail.com>
2016-10-02 20:15:05 +0900

    トランスパイラを簡単化。

--- src/orelang/Transpiler.java
+++ src/orelang/Transpiler.java
@@ -11,7 +11,6 @@ import net.arnx.jsonic.JSON;
 
 public class Transpiler {
 
-
 	public static Object transpile(Reader reader) throws IOException{
 		StringBuilder builder = new StringBuilder();
 	    BufferedReader br = new BufferedReader(reader);
@@ -23,21 +22,15 @@ public class Transpiler {
 	}
 
 	public static Object transpile(String script){
-		String ret = script;
-		ret = ret.replaceAll("\\(", "["); // ( -> [
-		ret = ret.replaceAll("\\)", "]"); // ( -> ]
-		ret = ret.replaceAll(";.*", ""); // remove comments
-		ret = ret.replaceAll("\n", ""); // remove line return
-		ret = ret.replaceAll("\\=", "\"=\""); // escape =
-		ret = ret.replaceAll("\\+", "\"+\""); // escape +
-		ret = ret.replaceAll("\\*", "\"*\""); // escape *
-		ret = ret.replaceAll("\\s\\s+", " "); // folds spaces into one space
-		ret = ret.replaceAll("\\s", ", "); // join by space with ,
-		ret = ret.replaceAll(",\\s*\\]", "]"); // remove comma of last elements [a, b, c,] -> [a, b, c
-		ret = ret.replaceAll("\\[(?!\")(([a-z]|[A-Z]|_)([a-z]|[A-Z]|[0-9]|_)*)", "[\"$1\""); // escape symbols
-		ret = ret.replaceAll("\\s(([a-z]|[A-Z]|_)([a-z]|[A-Z]|[0-9]|_)*)\\s", "\"$1\""); // escape symbols
-		ret = ret.replaceAll(",\\s(([a-z]|[A-Z]|_)([a-z]|[A-Z]|[0-9]|_)*)", ", \"$1\""); // escape symbols
-		return JSON.decode(ret);
+		String s = script;
+		s = s.replaceAll("\\(", "["); // ( -> [
+		s = s.replaceAll("\\)", "]"); // ( -> ]
+		s = s.replaceAll(";.*", ""); // remove comments
+		s = s.replaceAll("\n", ""); // remove line return
+		s = s.replaceAll("\\s+", ", "); // spaces -> ,
+		s = s.replaceAll("[+*=](?=[, \\]])", "\"$0\""); // escape symbols
+		s = s.replaceAll("[a-zA-Z_][a-zA-Z0-9_]*", "\"$0\""); // escape symbols
+		return JSON.decode(s);
 	}
 
 }
